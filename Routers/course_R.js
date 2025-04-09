e9743c7addcde13fb7eff153cf535665bc538693
MeitalHilf <tool16289@gmail.com>
2025-04-09 21:14:42 +0300

    הוספת קבצים ותיקיות חסרים לפרויקט

---
+++ Routers/course_R.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+const course_Mid = require("../middleware/course_Mid");
+
+router.get("/Add",(req,res)=>{
+    res.render("crs_add",{});
+});
+router.post("/Add", [course_Mid.AddCourse], (req, res) => {
+    res.send("ok");
+});
+router.get("/List",[course_Mid.GetAllCourses],(req,res)=>{
+    res.render("crs_list",{
+        page_title : "רשימת הקורסים",
+        courses : req.courses_data,
+    });
+});
